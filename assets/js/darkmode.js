9762d01ae7e4c92802f441a180749880f3917c9b
guangzheng.li <iguangzhengli@gmail.com>
2022-07-25 22:14:13 +0800

    feat: add blog comments using GitHub issues by utterances.

--- assets/js/darkmode.js
+++ assets/js/darkmode.js
@@ -10,17 +10,30 @@ if (mode !== null) {
       localStorage.setItem('theme', 'light');
       document.documentElement.removeAttribute('data-dark-mode');
     }
-
   })
 
   mode.addEventListener('click', () => {
     document.documentElement.toggleAttribute('data-dark-mode');
     localStorage.setItem('theme', document.documentElement.hasAttribute('data-dark-mode') ? 'dark' : 'light');
+    changeCommentsTheme();
   });
 
   if (localStorage.getItem('theme') === 'dark') {
     document.documentElement.setAttribute('data-dark-mode', '');
   } else {
     document.documentElement.removeAttribute('data-dark-mode');
+  }
+  changeCommentsTheme();
+}
+
+function changeCommentsTheme () {
+  if (document.querySelector('.utterances-frame')) {
+    const theme = localStorage.getItem('theme') === 'dark' ? 'github-dark' : 'github-light'
+    const message = {
+      type: 'set-theme',
+      theme: theme
+    };
+    const iframe = document.querySelector('.utterances-frame');
+    iframe.contentWindow.postMessage(message, 'https://utteranc.es');
   }
 }
